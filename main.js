458fffd419d16ad379201ce1e4d107df24781d0a
きみどり <syou.fuu.kai@gmail.com>
2026-07-07 18:00:20 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const problems = [{word:"5",read:"t",meanings:["5"]}]; //eruda.hide();
-console.log("main")
 if (!localStorage.hasOwnProperty("answer")){
 　localStorage.answer = "{}";
 };
@@ -9,6 +8,7 @@ if (!localStorage.hasOwnProperty("answerCount")){
 if (!localStorage.hasOwnProperty("likes")){
 　localStorage.likes = "[]";
 };
+const page = document.createElement("div");
 const startButton = document.createElement("div");
 const problemAmountBox = document.createElement("input");
 const text1 = document.createElement("div");
@@ -17,6 +17,8 @@ const text2 = document.createElement("div");
 const likeToggle = document.createElement("div");
 const text3 = document.createElement("div");
 const continueButton = document.createElement("div");
+page.id = "page";
+document.body.append(page);
 startButton.innerText = "Start";
 startButton.style.cssText = "border-radius:15px;color:white;text-align:center;font-size:xxx-large;border:solid;background-color:#0cc3ca;position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);width:80%;";
 startButton.id = "startButton";
@@ -82,10 +84,10 @@ startButton.addEventListener("click",c => {
 　　　};
 　　　if (isInclude){
 　　　　t.style.cssText = `${t.style.cssText}background-color:lime;`;
-　　　　document.body.append(t)
+　　　　add(t)
 　　　} else {
 　　　　t.style.cssText = `${t.style.cssText}background-color:red;`;
-　　　　document.body.append(t);
+　　　　add(t);
 　　　};
 　　});
 　　if (missMeaning.length > 0){
@@ -105,16 +107,16 @@ startButton.addEventListener("click",c => {
 　　read.style.cssText = `${read.style.cssText}position:relative;x-index:10;font-weight:1000;-webkit-text-stroke:0.01px black;`;
 　　duplicate.style.cssText = `${duplicate.style.cssText}position:relative;x-index:10;font-weight:1000;-webkit-text-stroke:0.01px black;`;
 　　answerCount.style.cssText = `${answerCount.style.cssText}position:relative;x-index:10;font-weight:1000;-webkit-text-stroke:0.01px black;`;
-　　document.body.append(word);
-　　document.body.append(meaning);
-　　document.body.append(read);
-　　document.body.append(debugButton);
-　　document.body.append(duplicate);
-　　document.body.append(answerCount);
+　　add(word);
+　　add(meaning);
+　　add(read);
+　　add(debugButton);
+　　add(duplicate);
+　　add(answerCount);
 　　return;
 　};
 　const likeToggle = document.getElementById("likeToggle")?.ariaLevel;
-　document.body.replaceChildren();
+　pageAllClear();
 　let newProblems = randomSort(problems);
 　if (likeToggle === "on") newProblems = randomSort(problems.filter(e => getStorage("likes").includes(e.word)));
 　if (Number.isFinite(problemLength) && problemLength > 0 && problemLength <= newProblems.length) newProblems.length = problemLength;
@@ -152,7 +154,7 @@ if (mostWrongProblems.length > 0){
 	button.addEventListener("click",ev => {
 		showProblem({newProblems:randomSort(mostWrongProblems),failureProblems:[],index:0,success:0,runCount:1,mode:document.getElementById("modeChangeButton").ariaLevel,mostWrongProblem:true});
 　});
-　document.body.append(button);
+　add(button);
 };
 likeToggle.addEventListener("click",ev => {
 　const t = ev.target;
@@ -172,9 +174,9 @@ likeToggle.addEventListener("click",ev => {
 　　box.value = `${length}`;
 　};
 　pushAnimation(t);
-　document.body.append(t);
-　document.body.append(text);
-　document.body.append(box);
+　add(t);
+　add(text);
+　add(box);
 });
 if (getStorage("playData")){
 　continueButton.id = "continueButton";
@@ -184,15 +186,15 @@ if (getStorage("playData")){
 　console.log(getStorage("playData"))
 　　showProblem(getStorage("playData"));
 　});
-　document.body.append(continueButton);
+　add(continueButton);
 };
 likeToggle.id = "likeToggle";
 likeToggle.ariaLevel = "off";
 likeToggle.innerText = "OFF";
 likeToggle.style.cssText = "align-content:center;height:30px;border-radius:15px;font-size:x-large;text-align:center;position:absolute;top:11%;left:20%;transform:translate(-50%,-50%);background-color:#ff9b00;border:solid;width:100px;color:white;";
 if (getStorage("likes").length > 0){
-　document.body.append(likeToggle);
-　document.body.append(text3);
+　add(likeToggle);
+　add(text3);
 };
 modeChangeButton.addEventListener("click",ev => {
 　const t = ev.target;
@@ -204,7 +206,7 @@ modeChangeButton.addEventListener("click",ev => {
 　　t.innerText = "ノーマル";
 　};
 　pushAnimation(t);
-　document.body.append(t);
+　add(t);
 });
 modeChangeButton.innerText = "ノーマル";
 modeChangeButton.style.cssText = "align-content:center;height:30px;border-radius:15px;color:white;background-color:#ff9b00;font-size:x-large;text-align:center;border:solid;position:absolute;top:11%;left:80%;transform:translate(-50%,-50%);width:100px;";
@@ -221,11 +223,11 @@ problemAmountBox.type = "number";
 problemAmountBox.id = "problemAmountBox";
 problemAmountBox.defaultValue = problems.length;
 problemAmountBox.style.cssText = "border-radius:15px;background-color:#dadedf;font-size:large;text-align:center;position:absolute;top:30%;left:50%;transform:translate(-50%,50%);";
-document.body.append(startButton);
-document.body.append(problemAmountBox);
-document.body.append(text1);
-document.body.append(modeChangeButton);
-document.body.append(text2);
+add(startButton);
+add(problemAmountBox);
+add(text1);
+add(modeChangeButton);
+add(text2);
 
 function showProblem(obj){
 let i = obj.index;
@@ -235,7 +237,7 @@ let success = obj.success;
 let runCount = obj.runCount;
 const mode = obj.mode;
 const likeProblem = obj.like;
-document.body.replaceChildren();
+pageAllClear();
 if (i < newProblems.length){
 　const word = document.createElement("div");
 　const textBox = document.createElement("input");
@@ -334,29 +336,29 @@ if (i < newProblems.length){
 　　　　if (likes.includes(nowWord)) likes = likes.filter(e => e !== nowWord);
 　　　};
 　　　pushAnimation(t);
-　　　document.body.append(t);
+　　　add(t);
 　　　setStorage("likes",likes);
 　　});
-　　document.body.append(answeredTextBox);
-　　document.body.append(button);
-　　document.body.append(meanings);
-　　document.body.append(answeredIndexText);
-　　document.body.append(nextButton);
-　　document.body.append(read);
-　　document.body.append(like);
+　　add(answeredTextBox);
+　　add(button);
+　　add(meanings);
+　　add(answeredIndexText);
+　　add(nextButton);
+　　add(read);
+　　add(like);
 　　setStorage("playData",newObj);
 　},{once:true});
 　indexText.innerText = `${i}/${newProblems.length}`;
 　indexText.cssText = "font-size:large;";
 　indexText.id = "indexText";
-　document.body.append(word);
-　document.body.append(textBox);
-　document.body.append(submitButton);
-　document.body.append(indexText);
+　add(word);
+　add(textBox);
+　add(submitButton);
+　add(indexText);
 　if (mode === "2"){
 　　const newWord = document.getElementById("word");
 　　newWord.style.cssText = `${newWord.style.cssText}transform:translate(-50%,-${newWord.getBoundingClientRect().height - 5}px)`;
-　　document.body.append(newWord);
+　　add(newWord);
 　};
 } else {
 　const text1 = document.createElement("div");
@@ -382,16 +384,16 @@ if (i < newProblems.length){
 　if (mode === "1") text5.innerText = "ノーマルモード";
 　　else text5.innerText = "日➡英モード";
 　if (likeProblem === true) text5.innerText = `${text5.innerText}★`;
-　document.body.append(text1);
-　document.body.append(text2);
-　document.body.append(text3);
-　document.body.append(text4);
-　document.body.append(text5);
+　add(text1);
+　add(text2);
+　add(text3);
+　add(text4);
+　add(text5);
 　if (obj.mostWrongProblem){
 　　const subTitle = document.createElement("div");
 　　subTitle.innerText = "最も間違えた問題";
 　　subTitle.style.cssText = "text-align:center;width:100%;font-size:xx-large;position:absolute;top:20%;left:50%;transform:translate(-50%,-50%);";
-　　document.body.append(subTitle);
+　　add(subTitle);
 　};
 　if (failureProblems.length > 0){
 　　const newObj2 = {newProblems:randomSort(failureProblems),failureProblems:[],index:0,success:0,runCount:++runCount,mode:mode,mostWrongProblem:obj.mostWrongProblem,like:likeProblem};
@@ -402,7 +404,7 @@ if (i < newProblems.length){
 　　newProblemButton.addEventListener("click",() => {
 　　　showProblem(newObj2);
 　　},{once:true});
-　　document.body.append(newProblemButton);
+　　add(newProblemButton);
 　　setStorage("playData",newObj2);
 　} else localStorage.removeItem("playData");
 };
@@ -437,5 +439,13 @@ return list;
 };
 
 function pushAnimation(t){
-　return t.animate([{offset:0,transform:"translate(-50%,-50%)"},{offset:0.2,scale:0.9,transform:"translate(-55%,-55%)"},{offset:0.6,scale:1.1,transform:"translate(-45%,-45%)"},{offset:1,scale:1,transform:"translate(-50%,-50%)"}],50);
+return t.animate([{offset:0,transform:"translate(-50%,-50%)"},{offset:0.2,scale:0.9,transform:"translate(-55%,-55%)"},{offset:0.6,scale:1.1,transform:"translate(-45%,-45%)"},{offset:1,scale:1,transform:"translate(-50%,-50%)"}],50);
+};
+
+function pageAllClear(){
+document.getElementById("page").replaceChildren();
+};
+
+function add(e){
+return document.body.append(document.getElementById("page").append(e));
 };
